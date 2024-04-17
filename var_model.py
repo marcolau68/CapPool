153adc2e3f4d5e4283f57a888947631bf3c9ceaf
Marco <marcolau68@gmail.com>
2024-04-17 11:38:30 +0100

    forecast VAR model for test set, kind of complete

--- var_model.py
+++ var_model.py
@@ -10,66 +10,71 @@ df.drop(columns=["Date"], inplace=True)
 
 # Set up VAR model and fit
 model = VAR(df[:constants.TRAIN_CUTOFF_INDEX]) 
-results = model.fit(2)
+results = model.fit(1)
 
 # Calculate predictions
 resid = results.resid
 pred_train = (df - resid).fillna(0)
 
-pred_test = results.forecast(df.values[constants.TRAIN_CUTOFF_INDEX-2:], 1507)
-# print(pred_test)
-print(pred_test.shape)
+pred_test = df.copy()
 
-predictions_test = df.copy()
-predictions_test.values[constants.TRAIN_CUTOFF_INDEX:] = pred_test
-print(predictions_test[constants.TRAIN_CUTOFF_INDEX:])
-print(pred_test[0])
-
-
-
+forecast = results.forecast(df.values[:constants.TRAIN_CUTOFF_INDEX], df.shape[0] - constants.TRAIN_CUTOFF_INDEX)
+pred_test[constants.TRAIN_CUTOFF_INDEX:] = forecast
 
 # Isolate evaluation tickers
 selected_columns = [ s + "_log_returns" for s in constants.EVAL_TICKERS ]
 
-# selected_resid = resid[selected_columns]
-# pred_train = pred_train[selected_columns]
-# true = df[selected_columns]
-# pred_test = pred_test[selected_columns]
-# # print(pred_test)
+selected_resid = resid[selected_columns]
+pred_train = pred_train[selected_columns]
+true = df[selected_columns]
+pred_test = pred_test[selected_columns]
 
-# # Calculate baseline prediction (probability of positive returns per day)
-# total = true.shape[0]
-# baseline_accuracy = true.gt(0).sum() / total
+# Calculate baseline prediction (probability of positive returns per day)
+total = true.shape[0]
+baseline_accuracy = true.gt(0).sum() / total
 
-# # Calculate VAR model accuracy in sample
-# compare_train = pred_train.where(pred_train * true > 0)
-# var_accuracy_train = compare_train.count() / total
+# Calculate VAR model accuracy in sample
+train_total = constants.TRAIN_CUTOFF_INDEX
+compare_train = pred_train[:constants.TRAIN_CUTOFF_INDEX].where(pred_train * true > 0)
+var_accuracy_train = compare_train.count() / train_total
 
-# # Calculate VAR model accuracy out of sample
-# compare_test = pred_test.where(pred_test * true > 0)
-# var_accuracy_test = compare_test.count() / total
+# Calculate VAR model accuracy out of sample
+test_total = df.shape[0] - constants.TRAIN_CUTOFF_INDEX
+compare_test = pred_test[constants.TRAIN_CUTOFF_INDEX:].where(pred_test * true > 0)
+var_accuracy_test = compare_test.count() / test_total
 
-# # Calculate VAR MSE
-# mse = (selected_resid ** 2).mean()
+# Calculate VAR MSE
+mse = (selected_resid ** 2).mean()
+
+mse_train = ((true[:constants.TRAIN_CUTOFF_INDEX] - pred_train[:constants.TRAIN_CUTOFF_INDEX]) ** 2).mean()
+mse_test = ((true[constants.TRAIN_CUTOFF_INDEX:] - pred_test[constants.TRAIN_CUTOFF_INDEX:]) ** 2).mean()
+
 
 # Print results
-# print("Baseline accuracy")
-# print("################################")
-# print(baseline_accuracy)
-# print("################################")
-# print("\n")
-# print("VAR Accuracy")
-# print("################################")
-# print(var_accuracy_train)
-# print(var_accuracy_test)
-# print("################################")
-# print("\n")
-# print("VAR Mean Squared Error")
-# print("################################")
-# print(mse)
-# print("################################")
-
-
+print("Baseline accuracy")
+print("################################")
+print(baseline_accuracy)
+print("################################")
+print("\n")
+print("VAR Accuracy Train")
+print("################################")
+print(var_accuracy_train)
+print("################################")
+print("\n")
+print("VAR Accuracy Test")
+print("################################")
+print(var_accuracy_test)
+print("################################")
+print("\n")
+print("VAR Mean Squared Error Train")
+print("################################")
+print(mse_train)
+print("################################")
+print("\n")
+print("VAR Mean Squared Error Test")
+print("################################")
+print(mse_test)
+print("################################")
 
 
 
